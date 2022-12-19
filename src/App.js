a02408c7106c84cf2e7e0822028653da2dae9d12
AngelGomez12 <gomez10828@gmail.com>
2022-12-19 16:15:56 -0300

    slider change

--- src/App.js
+++ src/App.js
@@ -11,16 +11,30 @@ import { Favoritos } from './components/Favoritos';
 
 
 function App() {
+  let token = sessionStorage.getItem('token')
   const [favorites, setFavorites] = useState([])
+  const [login, setLogin] = useState()
 
-    useEffect(() => {
-        const favsInLocal = localStorage.getItem('favs')
+  const loginEnter = () => {
+  let token = sessionStorage.getItem('token')
+  setLogin(token)
+    console.log(token);
+  }
+  
+  // useEffect(() => {
+  //   if (token) {
+  //     setLogin(token)
+  //   }
+  // }, [token])
 
-        if (favsInLocal !== null) {
-            const favsArray = JSON.parse(favsInLocal)
-            setFavorites(favsArray);
-        }
-    }, [])
+  useEffect(() => {
+    const favsInLocal = localStorage.getItem('favs')
+
+    if (favsInLocal !== null) {
+      const favsArray = JSON.parse(favsInLocal)
+      setFavorites(favsArray);
+    }
+  }, [])
 
   const favMovies = localStorage.getItem('favs')
 
@@ -32,7 +46,6 @@ function App() {
     tempMoviesInFavs = JSON.parse(favMovies)
   }
 
-  console.log(tempMoviesInFavs);
   const addOrRemoveFromFavs = e => {
     const btn = e.currentTarget
     const parent = btn.parentElement
@@ -63,13 +76,13 @@ function App() {
   }
   return (
     <div className='overflow-hidden'>
-      <Header favorites={favorites} />
+      <Header favorites={favorites} login={login}/>
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route exact path="/" render={() => <Login onClick={loginEnter} />} />
         <Route path="/listado" render={(props) => <Listado addOrRemoveFromFavs={addOrRemoveFromFavs} {...props} />} />
         <Route path="/detalle" component={Detalle} />
         <Route path="/resultados" render={(props) => <Resultados addOrRemoveFromFavs={addOrRemoveFromFavs} {...props} />} />
-        <Route path="/favoritos" render={(props) => <Favoritos favorites={favorites} addOrRemoveFromFavs={addOrRemoveFromFavs} {...props}  />} />
+        <Route path="/favoritos" render={(props) => <Favoritos favorites={favorites} addOrRemoveFromFavs={addOrRemoveFromFavs} {...props} />} />
       </Switch>
       <Footer />
     </div>
